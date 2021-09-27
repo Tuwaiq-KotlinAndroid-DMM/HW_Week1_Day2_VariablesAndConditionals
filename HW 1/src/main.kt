969958cce5cb83bc02e240719a13b0b719b61bcd
Mustafa <Mustafa.Haassen.dmm.k1@tuwaiq.edu.sa>
2021-09-27 16:57:35 +0300

    added project file

---
+++ HW 1/src/main.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var firstNum = 25
+    var sec2Num = 78
+    var th3Num = 87
+
+    if(firstNum < sec2Num){
+        if(sec2Num < th3Num){
+            println("$th3Num :is the gratest")
+        }
+    }
+
+    var A = 74
+    var B = 36
+    val sum = A + B
+    println(sum)
+}
