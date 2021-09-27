6d55bb5c1a0d4c7f87ba399c00754a5c5ef2bccc
WalaaAlshaikh <walaalshaikh94@gmail.com>
2021-09-27 19:22:11 +0300

    the solution of WH1

---
+++ solution/src/main/kotlin/solution.kt
@@ -0,0 +1,18 @@
+
+fun main(args: Array<String>) {
+    // the Solution of Task1
+    var num1 = 25
+    var num2 = 78
+    var num3 = 87
+    if (num1 >= num2 && num1 >= num3) {
+        println("the greatest : $num1")
+    }else if (num2 >= num1 && num2 >= num3){
+        println("the greatest :$num2")
+    }else println("the greatest: $num3")
+
+    // The solution of Task2
+    var n1= 74
+    var n2= 36
+    var sum= n1 + n2
+    println (sum)
+}
