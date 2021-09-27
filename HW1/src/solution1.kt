58a234633d7e60c67106177c7e7fad3e4b02be9a
fatimahmayouf <ftmmtf1995@gmail.com>
2021-09-27 22:27:36 +0300

    homework1

---
+++ HW1/src/solution1.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    println("input the 1st number: ")
+    var num1= readLine() !!.toInt()
+    println("input the 2nd number: ")
+    var num2= readLine() !!.toInt()
+    println("input the 3rd number: ")
+    var num3= readLine()!! .toInt()
+
+
+
+
+
+    if (num1>=num2){
+        if(num1>=num3){
+            println("the greatest number is $num1 ")
+
+        } }else if(num2>=num3){
+        println("the greatest number is $num2 ")
+    }else
+        println("the greatest number is $num3 ")
+}
---
+++ HW1/src/solution2.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    println("input 1st number")
+    var number1= readLine()!!.toInt()
+    println("input 2nd number")
+    var number2= readLine()!!.toInt()
+
+    println(number1 + number2)
+}
