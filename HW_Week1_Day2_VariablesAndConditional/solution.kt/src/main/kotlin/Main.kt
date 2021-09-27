1ce48fd719c84efd47ec2bcabe391888df46a804
waad <iwaad1414@gmail.com>
2021-09-27 19:49:40 +0300

    made an if and else if statment and add variables

---
+++ HW_Week1_Day2_VariablesAndConditional/solution.kt/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var number1: Int = 25
+    var number2: Int = 78
+    var number3: Int = 87
+    if (number3 == 87) {
+        println("the greates 87")
+
+    } else if (number2 == 78)
+        println(1)
+    else if (number1==25)
+        println(0)
+    var nm1=74
+    var nm2=36
+    println(nm1+nm2)
+
+}
