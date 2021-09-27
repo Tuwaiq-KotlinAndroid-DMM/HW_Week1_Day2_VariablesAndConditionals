8bcf5766adee151ae4862a42954db04b121c47d7
waad <iwaad1414@gmail.com>
2021-09-27 22:01:06 +0300

    edit my homework

---
+++ HW_Week1_Day2_VariablesAndConditional/sloutionkt/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var num1 = readLine()!!.toInt()
+    var num2 = readLine()!!.toInt()
+    var num3 = readLine()!!.toInt()
+    if(num3==num3)
+        println("the greatest")
+    else if (num3>=num1)
+        println("wrong")
+    else if (num1<=num2)
+        println("wrong1")
+
+}
