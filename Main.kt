688fd8ce8e92f67adbe32a08cc579eb28cd87631
ArwaSAO <91452271+ArwaSAO@users.noreply.github.com>
2021-09-28 06:59:25 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+
+
+    println("Enter the first number")
+var n1 = readLine()!!.toInt()
+    println("Enter the second number")
+    var n2 = readLine()!!.toInt()
+    println("Enter the third number")
+    var n3 = readLine()!!.toInt()
+
+    if (n1>=87){
+        println("the greatest number"+ n1)
+    } else if (n2<87){
+        println("second number" + n2)
+    }  else if (n3<87){
+        println("third number"+ n3)
+    }
+
+
+
+}
+
+
+
---
+++ part2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    var num1;Int = 74
+    var num2;Int = 36
+    var r : Int
+    r = num1 + num2
+    println(r)
+}
