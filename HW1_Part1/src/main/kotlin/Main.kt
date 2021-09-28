c9f812a6a97e8e6cc53081dc02950958bec48f1f
Saad alhajri <saadalhajri@Saads-MacBook-Air.local>
2021-09-28 09:08:29 +0300

    my homework

---
+++ HW1_Part1/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    var v1 = 25
+    var v2 = 78
+    var v3 = 87
+
+    if (v1 >= v3) {
+        println("The greatest: $v1")
+    } else if (v2 >= v3) {
+        print("The greatest: $v2")
+    } else {
+        println("The greatest: $v3")
+    }
+}
---
+++ HW1_Part2/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+  val a: Int = 74
+    val b: Int = 36
+val sum = a + b
+   println("$sum")}
