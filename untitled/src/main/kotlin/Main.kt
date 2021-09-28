7dfea709c47a0edcbee29b808995386842a57e68
Nourah Mohammed <n.alnasser.2014@gmail.com>
2021-09-28 08:47:33 +0300

    nourah HW

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+// println("enter your name:")
+//  var number = readLine()
+//  println("your name is :$name")
+//
+// speed >120
+//        if (speed>120){
+//         println ("camera on")
+
+ var a1 = 5
+ var a2 = 4
+ println(a1+a2)
+        }
