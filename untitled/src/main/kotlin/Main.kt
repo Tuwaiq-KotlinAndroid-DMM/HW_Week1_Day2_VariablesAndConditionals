b62cab84c3202f3933e9cc34d16f8e8fb4614c6e
hanaa <hana.alrashidi.dmm.k1@tuwaiq.edu.sa>
2021-09-28 12:42:12 +0300

    add file

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(args: Array<String>) {
+
+    val n1 = readLine()!!.toInt()
+
+    val n2 = readLine()!!.toInt()
+
+    val n3 = readLine()!!.toInt()
+
+    if (n3==n3)
+        println("the greatest")
+
+    else if (n3>=n1)
+        println("fals")
+
+    else if (n1<=n2)
+        println("fals")
+}
