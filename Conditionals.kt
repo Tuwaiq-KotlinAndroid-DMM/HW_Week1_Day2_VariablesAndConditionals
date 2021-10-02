09ac8cb0f8f512ece168e65f44e21178973c54d7
hanaa <hana.alrashidi.dmm.k1@tuwaiq.edu.sa>
2021-10-02 15:52:13 +0300

    corrected my answer

--- Conditionals.kt
+++ Conditionals.kt
@@ -1,4 +1,8 @@
-fun main(args: Array<String>) {
+fun main(){
+
+//Part I - Conditionals.
+
+    println("Enter the numbers ")
 
     val n1 = readLine()!!.toInt()
 
@@ -7,11 +11,21 @@ fun main(args: Array<String>) {
     val n3 = readLine()!!.toInt()
 
     if (n3==n3)
-        println("the greatest")
+        println("the greatest number is n3")
 
     else if (n3>=n1)
         println("fals")
 
     else if (n1<=n2)
         println("fals")
+
+
+//Part II - Variables
+
+    var a =74
+    var b =36
+    println(a+b)
+
+
+
 }
