7cbdf20b1315f5c8e514eef2a8186ded5a4ab8f3
Nourah Mohammed <n.alnasser.2014@gmail.com>
2021-10-03 01:46:27 +0300

    Nourah update my HWd2

---
+++ HomeworkD2/src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+//fun main(args: Array<String>) {
+//
+//    println("Enter First number: ")
+//
+//    var x : Int = readLine()!!.toInt()
+//
+//    println("Enter Second number: ")
+//
+//    var y : Int = readLine()!!.toInt()
+//
+//    println("Enter Third number: ")
+//
+//    var z : Int = readLine()!!.toInt()
+//
+//    println("Your numbers is: $x , $y , $z")
+//
+//    if (x > y && x > z)
+//    {
+//        println(x)
+//    }else if (y > x && y > z )
+//    {
+//        println(y)
+//    }else
+//    {
+//        println(z)
+//    }
+//
+//}
+//
+//
+//fun main(args: Array<String>) {
+//
+//    println("Enter First number: ")
+//    var x : Int = readLine()!!.toInt()
+//
+//    println("Enter Second number: ")
+//    var y : Int = readLine()!!.toInt()
+//
+//
+//    var sum : Int = x + y
+//
+//    println(sum)
+//
+//}
